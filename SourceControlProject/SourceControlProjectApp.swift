051d0e0330244a722b4adff09d4423473ce2643a
Aykutatabay <aykutatabay@outlook.com>
2024-10-28 21:12:55 +0300

    Initial Commit

---
+++ SourceControlProject/SourceControlProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlProjectApp.swift
+//  SourceControlProject
+//
+//  Created by Aykut ATABAY on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
