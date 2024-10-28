f8651e085af909775512ec1f8142b4f2f1137f78
Aykutatabay <aykutatabay@outlook.com>
2024-10-28 22:51:36 +0300

    Add HomeView

---
+++ SourceControlProject/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SourceControlProject
+//
+//  Created by Aykut ATABAY on 28.10.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
