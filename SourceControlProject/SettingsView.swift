49c902afa8194c980f6383f4438a584a265699b0
Aykutatabay <aykutatabay@outlook.com>
2024-10-30 00:11:02 +0300

    Added SettingsView

---
+++ SourceControlProject/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  SourceControlProject
+//
+//  Created by Aykut ATABAY on 30.10.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View!")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
